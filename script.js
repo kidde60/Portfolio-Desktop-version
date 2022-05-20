3b03c6b46e9f192994317cc060639c039b935431
kidde60 <kgeorgewilliam60@gmail.com>
2022-05-21 00:04:42 +0300

    Remove commented codes

--- script.js
+++ script.js
@@ -68,11 +68,6 @@ const cardArr = [
 
 const workSection = document.querySelector('.work-section');
 
-// card tempate
-// div = document.createElement('div');
-// div.className = 'placeholder1';
-// workSection.appendChild(div);
-// const placeholder1 = document.querySelector('.placeholder1');
 const placeholder1 = document.createElement('div');
 placeholder1.className = 'placeholder1';
 workSection.appendChild(placeholder1);
@@ -98,138 +93,6 @@ cardArr.forEach((item, i) => {
   const paragraph = document.createElement('p');
   paragraph.textContent = cardArr[i].brief;
   stories.appendChild(paragraph);
-
-  // ul = document.createElement('ul');
-  // ul.className = 'items';
-  // ul.style.margin = '0';
-  // stories.appendChild(ul);
-  // li = document.createElement('li');
-  // li.className = 'tech';
-  // let [first, second, third, forth] = cardArr[i].technologies;
-  // li.textContent = first;
-  // ul.appendChild(li);
-  // li = document.createElement('li');
-  // li.className = 'tech';
-  // li.textContent = second;
-  // ul.appendChild(li);
-  // li = document.createElement('li');
-  // li.className = 'tech';
-  // li.textContent = third;
-  // ul.appendChild(li);
-  // li = document.createElement('li');
-  // li.className = 'tech';
-  // li.textContent = forth;
-  // ul.appendChild(li);
-  // const button = document.createElement('button');
-  // button.className = 'button';
-  // button.textContent = 'See project';
-  // button.name = 'details';
-  // button.type = 'button';
-  // button.id = cardArr[i].id;
-  // stories.appendChild(button);
-  // // Card Button function
-  // function openPopup(e) {
-  //   const j = e.target.id;
-  //   const popup = document.createElement('div');
-  //   popup.className = 'popup';
-  //   workSection.appendChild(popup);
-
-  //   function closePopup() {
-  //     popup.style.display = 'none';
-  //   }
-
-  //   const popupContent = document.createElement('div');
-  //   popupContent.className = 'popupContent fontSmall';
-  //   popup.appendChild(popupContent);
-
-  //   img = document.createElement('img');
-  //   img.src = 'images/Icon.svg';
-  //   img.className = 'close';
-  //   popupContent.appendChild(img);
-  //   img.addEventListener('click', closePopup);
-
-  //   const popupHeader = document.createElement('h2');
-  //   popupHeader.textContent = cardArr[j].name;
-  //   popupContent.appendChild(popupHeader);
-
-  //   const popupSnapshot = document.createElement('img');
-  //   popupSnapshot.src = cardArr[j].image;
-  //   popupSnapshot.className = 'crop';
-  //   popupContent.appendChild(popupSnapshot);
-  //   const desktopPopup = document.createElement('div');
-  //   desktopPopup.className = 'select';
-  //   popupContent.appendChild(desktopPopup);
-  //   const popupParagraph = document.createElement('p');
-  //   popupParagraph.className = 'popupParagraph';
-  //   popupParagraph.textContent = cardArr[j].description;
-  //   desktopPopup.appendChild(popupParagraph);
-  //   const desktopRight = document.createElement('div');
-  //   desktopRight.className = 'desktopRight';
-  //   desktopPopup.appendChild(desktopRight);
-  //   ul = document.createElement('ul');
-  //   ul.className = 'lng liststyle';
-  //   ul.style.marginBottom = '15px';
-  //   desktopRight.appendChild(ul);
-  //   [first, second, third, forth] = cardArr[j].technologies;
-  //   li = document.createElement('li');
-  //   li.className = 'tech';
-  //   li.textContent = first;
-  //   ul.appendChild(li);
-  //   li = document.createElement('li');
-  //   li.className = 'tech';
-  //   li.textContent = second;
-  //   ul.appendChild(li);
-  //   li = document.createElement('li');
-  //   li.className = 'tech';
-  //   li.textContent = third;
-  //   ul.appendChild(li);
-  //   li = document.createElement('li');
-  //   li.className = 'tech';
-  //   li.textContent = forth;
-  //   ul.appendChild(li);
-  //   const buttonBar = document.createElement('form');
-  //   buttonBar.className = 'button';
-  //   desktopRight.appendChild(buttonBar);
-
-  //   const live = document.createElement('button');
-  //   live.className = 'btn fontSmall';
-  //   live.id = 'live';
-  //   live.textContent = 'Live vesrion';
-  //   buttonBar.appendChild(live);
-
-  //   img = document.createElement('img');
-  //   img.src = 'img/live.svg';
-  //   img.className = 'popupImg';
-  //   live.appendChild(img);
-  //   const source = document.createElement('button');
-  //   source.className = 'btn fontSmall';
-  //   source.id = 'source';
-  //   source.textContent = 'See source';
-  //   buttonBar.appendChild(source);
-
-  //   img = document.createElement('img');
-  //   img.src = 'img/gthb.svg';
-  //   img.className = 'popupImg';
-  //   source.appendChild(img);
-  //   function goToLive() {
-  //     window.open(cardArr[0].liveLink);
-  //   }
-  //   function goToSource() {
-  //     window.open(cardArr[0].sourceLink);
-  //   }
-  //   live.addEventListener('click', goToLive);
-  //   source.addEventListener('click', goToSource);
-
-  //   if (window.innerWidth < 768) {
-  //     popupParagraph.classList.remove('popupParagraph');
-  //     popupParagraph.classList.remove('desktopRight');
-  //   }
-  //   popup.style.display = 'block';
-  //   popup.style.overflow = 'auto';
-  // }
-
-  // stories.appendChild(button);
-  // button.addEventListener('click', openPopup);
 });
 // contact form validation
 const contactForm = document.querySelector('.form');
@@ -238,8 +101,6 @@ const validationMassege = contactForm.querySelector('.error');
 validationMassege.style.color = 'white';
 validationMassege.style.textAlign = 'center';
 validationMassege.style.fontSize = '20px';
-validationMassege.style.padding = '10px';
-validationMassege.style.borderRadius = '2px';
 
 contactForm.addEventListener('submit', (event) => {
   let isValid = false;
@@ -248,7 +109,7 @@ contactForm.addEventListener('submit', (event) => {
   }
   if (isValid === false) {
     event.preventDefault();
-    validationMassege.innerText = 'The email field should be in lowercase only.';
+    validationMassege.innerText = 'The email field must be in lowercase only.';
     setTimeout(() => {
       validationMassege.textContent = '';
     }, 5000);
